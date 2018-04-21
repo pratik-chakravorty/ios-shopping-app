2cc6fc4b346893897faf377bb55380b1fd5a15e4
Pratik Chakravorty <psman1253@gmail.com>
2018-04-21 17:36:59 +0100

    Changed

--- ShoppingApp/CollectionViewCell.swift
+++ ShoppingApp/CollectionViewCell.swift
@@ -2,8 +2,8 @@
 //  CollectionViewCell.swift
 //  ShoppingApp
 //
-//  Created by Mike Glover on 05/04/2018.
-//  Copyright © 2018 Mike Glover. All rights reserved.
+//  Created by 117222405 on 05/04/2018.
+//  Copyright © 2018 117222405. All rights reserved.
 //
 
 import UIKit
